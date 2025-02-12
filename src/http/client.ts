3aa96faa1930cf5ecc043d7953f39d7072b00b25
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2025-02-12 21:35:20 +0600

    add refreshtoken system automatic refresh for user authanticate

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "../store";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
@@ -8,3 +9,34 @@ export const api = axios.create({
     Accept: "application/json",
   },
 });
+
+const refreshToken = async () => {
+  await axios.post(
+    `${import.meta.env.VITE_BACKEND_URL}/auth/refresh`,
+    {},
+    {
+      withCredentials: true,
+    }
+  );
+};
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const orginalerror = error.config;
+
+    if (error.response.status === 401 && !orginalerror._isRetry) {
+      try {
+        orginalerror._isRetry = true
+        const headers = { ...orginalerror.headers };
+        await refreshToken();
+        return api.request({ ...orginalerror, headers });
+      } catch (error) {
+        console.log("refresh Token error", error);
+        useAuthStore.getState().logout();
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -3,6 +3,7 @@ import { self } from "../http/api";
 import { useQuery } from "@tanstack/react-query";
 import { useAuthStore } from "../store";
 import { Outlet } from "react-router";
+import { AxiosError } from "axios";
 
 export default function Root() {
   const getself = async () => {
@@ -14,15 +15,23 @@ export default function Root() {
   const { data } = useQuery({
     queryKey: ["self"],
     queryFn: getself,
+    retry: (failurCount: number, error) => {
+      if (error instanceof AxiosError && error.response?.status === 401) {
+        return false;
+      }
+      return failurCount < 3
+    },
   });
   useEffect(() => {
-    console.log(data);
-    if(  data?.data) {
-      setUser(data?.data)
+    if (data?.data) {
+      setUser(data?.data);
     }
-  }, [data,setUser,]);
+  }, [data, setUser]);
 
-  return <div>Root file
-    <Outlet/> 
-  </div>;
+  return (
+    <div>
+      Root file
+      <Outlet />
+    </div>
+  );
 }
