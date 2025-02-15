6c01020b142bfe0b23cdf07f9af7b1362b97afff
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2025-02-15 23:06:41 +0600

    data fetching

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,13 +1,35 @@
 import { RightOutlined } from "@ant-design/icons";
+import { useQuery } from "@tanstack/react-query";
 import { Breadcrumb } from "antd";
-import React from "react";
+import React, { use } from "react";
+import { getUserdata } from "../http/api";
+import { Users } from "../types";
 
-export default function user() {
+export default function User() {
+  const {data:users} = useQuery({
+    queryKey:["users"],
+    queryFn: () => {
+      return getUserdata().then((res) => res.data);
+    }
+})
   return (
     <>
       <Breadcrumb separator={<RightOutlined  style={{ fontSize: "16px", }}/>} items={[{ title: "Dashboard" }, {title:"User"}
-
       ]}></Breadcrumb>
+
+      <div>
+        <ul>
+          {users?.map((user: Users) => (
+             <li>
+               <p>{user.id}</p>
+               <p>{user.firstname}</p>
+               <p>{user.lastname}</p>
+               <p>{user.email}</p>
+               <p>{user.createdAt}</p>
+             </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,11 @@ export type Credentials = {
   email: string;
   password: string;
 };
+
+export type Users = {
+  id:number,
+  firstname:string,
+  lastname:string,
+  email:string,
+  createdAt:string
+}
