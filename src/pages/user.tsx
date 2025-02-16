f959c739f68712ce07b94032e5dac4c291c1370e
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2025-02-16 17:57:53 +0600

    styleing tabel

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,35 +1,50 @@
 import { RightOutlined } from "@ant-design/icons";
 import { useQuery } from "@tanstack/react-query";
-import { Breadcrumb } from "antd";
+import { Breadcrumb, Space, Table } from "antd";
 import React, { use } from "react";
 import { getUserdata } from "../http/api";
 import { Users } from "../types";
+ 
 
 export default function User() {
-  const {data:users} = useQuery({
-    queryKey:["users"],
+  const columns = [
+
+    {
+      title: "FirstName",
+      dataIndex: "lastname",
+      key: "lastname",
+      render: (_text: string, record: Users ) => <div>
+      {record.firstname} {record.lastname}
+      </div>,
+    },
+    
+    {
+      title: "Email",
+      dataIndex:"email",
+      key: "email",
+    },
+    {
+      title: "Role",
+      dataIndex: "role",
+      key: "role",
+    },
+  ];
+  const { data: users } = useQuery({
+    queryKey: ["users"],
     queryFn: () => {
       return getUserdata().then((res) => res.data);
-    }
-})
+    },
+  });
   return (
     <>
-      <Breadcrumb separator={<RightOutlined  style={{ fontSize: "16px", }}/>} items={[{ title: "Dashboard" }, {title:"User"}
-      ]}></Breadcrumb>
-
+     <Space direction="vertical" style={{width:"100%"}}> <Breadcrumb
+        separator={<RightOutlined style={{ fontSize: "16px" }} />}
+        items={[{ title: "Dashboard" }, { title: "User" }]}
+      ></Breadcrumb>
+     
       <div>
-        <ul>
-          {users?.map((user: Users) => (
-             <li>
-               <p>{user.id}</p>
-               <p>{user.firstname}</p>
-               <p>{user.lastname}</p>
-               <p>{user.email}</p>
-               <p>{user.createdAt}</p>
-             </li>
-          ))}
-        </ul>
-      </div>
+         <Table columns={columns} dataSource={users}></Table>
+      </div></Space>
     </>
   );
 }
