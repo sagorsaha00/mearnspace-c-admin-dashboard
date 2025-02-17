3d7dd7d46138662f735fe3e43df94f44905aece0
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2025-02-18 01:13:48 +0600

    validarion add in createuser components

--- src/pages/user/createUser.tsx
+++ src/pages/user/createUser.tsx
@@ -3,6 +3,7 @@ import { Card, Col, Form, Input, Row, Select, Space } from "antd";
 import React from "react";
 import { getAlltanentsdata } from "../../http/api";
 import { Tanent } from "../../types";
+import Password from "antd/es/input/Password";
 
 export default function CreateUser() {
   const { data: tanents = [] } = useQuery({
@@ -20,17 +21,36 @@ export default function CreateUser() {
           <Card title="basic info" bordered={false}>
             <Row gutter={20}>
               <Col span={12}>
-                <Form.Item label="First Name" name="firstname">
+                <Form.Item
+                  label="First Name"
+                  name="firstname"
+                  rules={[
+                    { required: true, message: "Please input your Firstname!" },
+                  ]}
+                >
                   <Input placeholder="firstname" />
                 </Form.Item>
               </Col>
               <Col span={12}>
-                <Form.Item label="Last Name" name="lasatname">
+                <Form.Item
+                  label="Last Name"
+                  name="lasatname"
+                  rules={[
+                    { required: true, message: "Please input your Lastname!" },
+                  ]}
+                >
                   <Input placeholder="lastname" />
                 </Form.Item>
               </Col>
               <Col span={12}>
-                <Form.Item label="Email" name="Email">
+                <Form.Item
+                  label="Email"
+                  name="Email"
+                  rules={[
+                    { required: true, message: "Please input your Email!" },
+                    {type:'email', message:"please input a valid a email"}
+                  ]}
+                >
                   <Input placeholder="email" />
                 </Form.Item>
               </Col>
@@ -39,7 +59,9 @@ export default function CreateUser() {
           <Card title="Sequrity info" bordered={false}>
             <Row gutter={20}>
               <Col span={12}>
-                <Form.Item label="Passwrod" name="passwrod">
+                <Form.Item label="Passwrod" name="passwrod" rules={[
+                    { required: true, message: "Please input your Password!" },
+                  ]}>
                   <Input type="password" placeholder="password" />
                 </Form.Item>
               </Col>
@@ -48,7 +70,9 @@ export default function CreateUser() {
           <Card title="Role info" dir="horizontal" bordered={false}>
             <Row gutter={20}>
               <Col span={12}>
-                <Form.Item label="Role" name="Role">
+                <Form.Item label="Role" name="Role" rules={[
+                    { required: true, message: "Please input your Role!" },
+                  ]}>
                   <Select
                     style={{ width: "80%" }}
                     allowClear={true}
@@ -61,18 +85,21 @@ export default function CreateUser() {
                 </Form.Item>
               </Col>
               <Col span={12}>
-                <Form.Item label="Resturant" name="resturant">
+                <Form.Item label="Resturant" name="resturant" rules={[
+                    { required: true, message: "Please input your Resturant!" },
+                  ]}>
                   <Select
                     style={{ width: "80%" }}
                     allowClear={true}
                     placeholder="Status"
                   >
-                    {
-                        tanents.map((tanent:Tanent) => {
-                            return <Select.Option value={tanent.id}>{tanent.name}</Select.Option>
-                        })
-                    }
-                   
+                    {tanents.map((tanent: Tanent) => {
+                      return (
+                        <Select.Option value={tanent.id}>
+                          {tanent.name}
+                        </Select.Option>
+                      );
+                    })}
                   </Select>
                 </Form.Item>
               </Col>
